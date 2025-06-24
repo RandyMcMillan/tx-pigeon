d34ecf76fa369e4383bdd698595b6c45b392f3b1
randymcmillan <randymcmillan@protonmail.com>
2025-06-24 18:09:17 -0400

    src/prelude/ui:get_time

--- src/prelude/ui/mod.rs
+++ src/prelude/ui/mod.rs
@@ -1,3 +1,5 @@
+use crate::prelude::handlers::config::CompleteConfig;
+use chrono::{DateTime, Utc};
 use tui::{
     backend::Backend,
     layout::{Constraint, Direction, Layout},
@@ -6,7 +8,12 @@ use tui::{
     widgets::{Block, Borders, Paragraph},
 };
 
-use crate::prelude::handlers::config::CompleteConfig;
+pub fn get_time() -> String {
+    let now_utc: DateTime<Utc> = Utc::now();
+
+    let default_string = now_utc.to_string();
+    return format!("Default format: {}", default_string);
+}
 
 pub fn draw_ui<T: Backend>(frame: &mut Frame<T>, config: &CompleteConfig) {
     let vertical_chunk_constraints = vec![Constraint::Percentage(50), Constraint::Percentage(50)];
@@ -26,7 +33,11 @@ pub fn draw_ui<T: Backend>(frame: &mut Frame<T>, config: &CompleteConfig) {
 
     frame.render_widget(table, vertical_chunks[0]);
 
-    let default_message = Text::from(String::from(config.frontend.default_message.to_owned()));
+    use bitcoin::{Network, Transaction};
+    use rust_mempool::MempoolClient;
+    let client = MempoolClient::new(Network::Bitcoin);
+
+    let default_message = Text::from(String::from(get_time()));
 
     let vertical_chunks = Layout::default()
         .direction(Direction::Vertical)
